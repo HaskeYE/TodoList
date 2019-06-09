879de33c6673cd0bd134cd6be379baa1340f3595
HaskeYE <yegor475@bk.ru>
2019-06-09 22:24:25 +0300

    маленький фикс

--- app/src/main/java/com/example/todo/TodayList.java
+++ app/src/main/java/com/example/todo/TodayList.java
@@ -18,7 +18,7 @@ public class TodayList extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+        //Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_today_list, container, false);
         //Список дел на сегодня
         ListView listView = view.findViewById(R.id.todoToday);
