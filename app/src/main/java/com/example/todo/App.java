e028591d1a77fd34616b8d7844be298b533ffc1c
Petr <4petrain9@gmail.com>
2019-06-12 00:17:36 +0300

    gogogo

---
+++ app/src/main/java/com/example/todo/App.java
@@ -0,0 +1,70 @@
+package com.example.todo;
+
+import android.app.Application;
+
+import androidx.room.*;
+
+import java.util.List;
+
+public class App extends Application {
+    private static App instance;
+    private AppDatabase database;
+
+    @androidx.room.Database(entities = {Ideas.class}, version = 1)
+    public abstract class AppDatabase extends RoomDatabase {
+        public abstract IdeasDao ideasDao();
+    }
+
+    @Entity
+    public class Ideas {
+        @PrimaryKey
+        public long id;
+
+        public String head;
+
+        public String data;
+
+        public String time;
+    }
+
+    @Dao
+    public interface IdeasDao {
+        @Query("SELECT * FROM Ideas")
+        List<Ideas> getAll();
+
+        @Query("SELECT * FROM Ideas WHERE id = :id")
+        Ideas getById(long id);
+
+        @Query("SELECT * FROM Ideas WHERE head = :head")
+        List<Ideas> getByHead(String head);
+
+        @Query("SELECT * FROM Ideas WHERE data = :data")
+        List<Ideas> getByData(String data);
+
+        @Insert
+        void insert(Ideas idea);
+
+        @Update
+        void update(Ideas idea);
+
+        @Delete
+        void delete(Ideas idea);
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        instance = this;
+        database = Room.databaseBuilder(getApplicationContext(), AppDatabase.class, "database")
+                .allowMainThreadQueries()
+                .build();
+    }
+
+    public App getInstance() {
+        return instance;
+    }
+
+    public AppDatabase getDatabase() {
+        return database;
+    }
+}
